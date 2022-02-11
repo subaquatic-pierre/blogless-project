79a1dae07e49231487ef5ec3d3c24003804bcae3
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-02-11 19:46:53 +0400

    Start adding api lambda fuctions

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,38 +1,18 @@
-# ------
-# General Variables
-# ------
-variable "tags" {
-  type        = map(string)
-  description = "Tags"
-}
-
 variable "aws_account_id" {}
 variable "github_account" {}
 
 # ------
 # Repositories
 # ------
-variable "frontend_github_repo" {
+variable "github_repo" {
   type        = map(string)
-  description = "Storefront repository"
-}
-
-variable "api_github_repo" {
-  type        = map(string)
-  description = "API repository"
-}
-
-# ------
-# ECS Variables
-# ------
-variable "api_ecr_app_uri" {
-  type        = string
-  description = "API App ECR repository uri"
+  description = "Repo with frontend code"
 }
 
 # ------
 # Domain Variables
 # ------
+
 variable "domain_name" {
   type        = string
   description = "Root Route53 domain name"
@@ -48,24 +28,5 @@ variable "ssl_cert_arn" {
 # ------
 variable "vpc_cidr" {}
 
-variable "private_subnet_cidrs" {
-  type = list(any)
-}
-
-variable "public_subnet_cidrs" {
-  type = list(any)
-}
-
-variable "availability_zones" {
-  type = list(any)
-}
-
-# ------
-# Build Secrets
-# ------
-variable "build_secrets" {
-  type        = map(string)
-  description = "All secrets used in building images"
-}
 
 
