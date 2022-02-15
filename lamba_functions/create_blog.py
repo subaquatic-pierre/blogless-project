457559d417d8dbf74a54b63d51b1b81b54278e88
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-02-15 14:36:44 +0400

    Completed delete blog

--- lamba_functions/create_blog.py
+++ lamba_functions/create_blog.py
@@ -5,9 +5,9 @@ import boto3
 
 s3 = boto3.client("s3")
 BUCKET = "serverless-blog-contents"
-BLOG_BASE_KEY = "blog"
+BLOG_BASE_KEY = "blog/"
 
-index_response = s3.get_object(Bucket=BUCKET, Key=f"{BLOG_BASE_KEY}/index.json")
+index_response = s3.get_object(Bucket=BUCKET, Key=f"{BLOG_BASE_KEY}index.json")
 
 # Get current index
 index: list = json.loads(index_response["Body"].read())
@@ -15,9 +15,9 @@ index: list = json.loads(index_response["Body"].read())
 
 # Define new blog data
 new_blog_id = len(index)
-blog_name = generate_name(style="capital")
+blog_title = generate_name(style="capital")
 
-new_dir_key = f'{BLOG_BASE_KEY}/{new_blog_id}-{blog_name.lower().replace(" ", "-")}'
+new_dir_key = f'{BLOG_BASE_KEY}{new_blog_id}-{blog_title.lower().replace(" ", "-")}/'
 time_stamp = int(time.time())
 
 file = open("blog_template/content.json")
@@ -27,23 +27,22 @@ file.close()
 meta_data = {
     "id": new_blog_id,
     "time_stamp": time_stamp,
-    "title": blog_name,
+    "title": blog_title,
     "dir_key": new_dir_key,
 }
 
 # upload meta data
-s3.put_object(Bucket=BUCKET, Key=f"{new_dir_key}/meta.json", Body=json.dumps(meta_data))
+s3.put_object(Bucket=BUCKET, Key=new_dir_key)
+s3.put_object(Bucket=BUCKET, Key=f"{new_dir_key}meta.json", Body=json.dumps(meta_data))
 
 # upload contents
-s3.put_object(
-    Bucket=BUCKET, Key=f"{new_dir_key}/content.json", Body=json.dumps(content)
-)
+s3.put_object(Bucket=BUCKET, Key=f"{new_dir_key}content.json", Body=json.dumps(content))
 
 # upload images
-s3.put_object(Bucket=BUCKET, Key=f"{new_dir_key}/images/")
+s3.put_object(Bucket=BUCKET, Key=f"{new_dir_key}images/")
 
 
 index.append(meta_data)
 
 # update index
-s3.put_object(Bucket=BUCKET, Key=f"{BLOG_BASE_KEY}/index.json", Body=json.dumps(index))
+s3.put_object(Bucket=BUCKET, Key=f"{BLOG_BASE_KEY}index.json", Body=json.dumps(index))
--- lamba_functions/read_blog.py
+++ lamba_functions/read_blog.py
@@ -5,24 +5,22 @@ import boto3
 
 s3 = boto3.client("s3")
 BUCKET = "serverless-blog-contents"
-BLOG_BASE_KEY = "blog"
+BLOG_BASE_KEY = "blog/"
 
-index_response = s3.get_object(Bucket=BUCKET, Key=f"{BLOG_BASE_KEY}/index.json")
+index_response = s3.get_object(Bucket=BUCKET, Key=f"{BLOG_BASE_KEY}index.json")
 
 index = json.loads(index_response["Body"].read())
 
-blog_id = 4
+blog_id = 1
 
 blog_data = next((blog for blog in index if blog["id"] == blog_id), None)
 
 try:
-    blog_meta_res = s3.get_object(
-        Bucket=BUCKET, Key=f"{blog_data['dir_key']}/meta.json"
-    )
+    blog_meta_res = s3.get_object(Bucket=BUCKET, Key=f"{blog_data['dir_key']}meta.json")
     blog_meta = json.loads(blog_meta_res["Body"].read())
 
     blog_content_res = s3.get_object(
-        Bucket=BUCKET, Key=f"{blog_data['dir_key']}/content.json"
+        Bucket=BUCKET, Key=f"{blog_data['dir_key']}content.json"
     )
     blog_content = json.loads(blog_content_res["Body"].read())
 
