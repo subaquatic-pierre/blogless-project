d695f1a073185b356b2e8043a8c968da9a075dfa
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-03-17 12:18:25 +0400

    Completed get single blog lambda and api endpoint

--- infrastructure/lambda/functions/get.py
+++ infrastructure/lambda/functions/get.py
@@ -4,8 +4,11 @@ from postmanager.response import Response
 
 
 def list(event, context):
-    bucket_proxy = BucketProxy("serverless-blog-contents", "blog/")
-    post_manager = PostManager("Blog", bucket_proxy)
+    path = event.get("path")
+    template_str = path.split("/")[1]
+    template_name = template_str.capitalize()
+    bucket_proxy = BucketProxy("serverless-blog-contents", f"{template_str}/")
+    post_manager = PostManager(template_name, bucket_proxy)
 
     response = Response(post_manager.index)
 
@@ -13,7 +16,20 @@ def list(event, context):
 
 
 def get(event, context):
-    body = {"event": event, "context": context}
+    path = event.get("path")
+    template_str = path.split("/")[1]
+    template_name = template_str.capitalize()
+
+    print(path)
+
+    bucket_proxy = BucketProxy("serverless-blog-contents", f"{template_str}/")
+    post_manager = PostManager(template_name, bucket_proxy)
+
+    blog_id = path.split("/")[-1]
+
+    post = post_manager.get_by_id(int(blog_id))
+
+    body = post.to_json()
     response = Response(body)
 
     return response.to_json()
