09b5ac9b40ca6a2cdfbc45018fc018bb0279eaed
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-03-19 12:34:04 +0400

    Removed unused types

--- frontend/src/@types/Marketplace.d.ts
+++
@@ -1,5 +0,0 @@
-interface IMarketplaceItem {
-  tokenId: string | null;
-  tokenUri: string | null;
-  listingInfo: IListingInfo;
-}
---
+++ frontend/src/@types/Notification.d.ts
@@ -0,0 +1,5 @@
+interface INotificationState {
+  isOpen: boolean;
+  message: string;
+  color: AlertColor;
+}
