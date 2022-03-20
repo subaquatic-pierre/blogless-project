0a27484fee9824cd5a59cb8262d7d7a7216c8a91
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-03-20 13:18:47 +0400

    Added aditor JS to create page

--- frontend/src/pages/Create.tsx
+++ frontend/src/pages/Create.tsx
@@ -1,25 +1,41 @@
 import React from "react";
 
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
 import Page from "components/Page";
 import PageHeading from "components/PageHeading";
 import Heading from "components/Heading";
 
-const Create = () => {
-  const [quillValue, setQuillValue] = React.useState("");
+import { createReactEditorJS } from "react-editor-js";
 
-  const handleEditorStateChange = (state) => {
-    setQuillValue(state);
-    console.log(state);
-  };
+const ReactEditorJS = createReactEditorJS();
+
+const Create = () => {
+  const editorCore = React.useRef(null);
+
+  const handleInitialize = React.useCallback((instance) => {
+    editorCore.current = instance;
+  }, []);
+
+  const handleSave = React.useCallback(async () => {
+    const savedData = await editorCore.current.save();
+    console.log(savedData);
+  }, []);
 
   return (
     <Page>
       <PageHeading title="Create Post" />
       <Box sx={{ my: 2 }}>
         <Heading title="Editor JS"></Heading>
-        <Box></Box>
+        <Box>
+          <ReactEditorJS defaultValue={[]} onInitialize={handleInitialize} />
+        </Box>
+      </Box>
+      <Box>
+        <Button variant="contained" onClick={handleSave}>
+          Save
+        </Button>
       </Box>
     </Page>
   );
