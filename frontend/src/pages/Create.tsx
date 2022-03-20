e9ef0f9e618968c5203775c1f8f9e31601bc5759
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-03-20 12:53:52 +0400

    Removed Quill and draft js

--- frontend/src/pages/Create.tsx
+++ frontend/src/pages/Create.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 
+import Box from "@mui/material/Box";
+
 import Page from "components/Page";
 import PageHeading from "components/PageHeading";
+import Heading from "components/Heading";
 
 const Create = () => {
+  const [quillValue, setQuillValue] = React.useState("");
+
+  const handleEditorStateChange = (state) => {
+    setQuillValue(state);
+    console.log(state);
+  };
+
   return (
     <Page>
       <PageHeading title="Create Post" />
+      <Box sx={{ my: 2 }}>
+        <Heading title="Editor JS"></Heading>
+        <Box></Box>
+      </Box>
     </Page>
   );
 };
