24d9467448b700e6c4b278f56df8c6f7f593d5dd
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-03-21 14:33:46 +0400

    Started adding formik to create page

--- frontend/src/pages/Create.tsx
+++ frontend/src/pages/Create.tsx
@@ -1,18 +1,32 @@
 import React from "react";
 
+import { createReactEditorJS } from "react-editor-js";
+import { Formik, Field, Form, FormikHelpers } from "formik";
+
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
 
 import Page from "components/Page";
 import PageHeading from "components/PageHeading";
 import Heading from "components/Heading";
 
-import { createReactEditorJS } from "react-editor-js";
+import { EDITOR_JS_TOOLS } from "tools";
 
 const ReactEditorJS = createReactEditorJS();
 
+interface IFormValues {
+  title: string;
+}
+
+const initialFormValues: IFormValues = {
+  title: "",
+};
+
 const Create = () => {
   const editorCore = React.useRef(null);
+  const [submitting, setSubmitting] = React.useState(false);
 
   const handleInitialize = React.useCallback((instance) => {
     editorCore.current = instance;
@@ -23,15 +37,36 @@ const Create = () => {
     console.log(savedData);
   }, []);
 
+  const handleFormSubmit = (values: IFormValues) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 500);
+  };
+
   return (
     <Page>
       <PageHeading title="Create Post" />
-      <Box sx={{ my: 2 }}>
-        <Heading title="Editor JS"></Heading>
-        <Box>
-          <ReactEditorJS defaultValue={[]} onInitialize={handleInitialize} />
+      <Paper sx={{ my: 2, p: 2 }}>
+        <Formik initialValues={initialFormValues} onSubmit={handleFormSubmit}>
+          <Form>
+            <TextField
+              id="outlined-basic"
+              label="Outlined"
+              variant="outlined"
+            />
+
+            <button type="submit">Submit</button>
+          </Form>
+        </Formik>
+        <Box sx={{ my: 2 }}>
+          <ReactEditorJS
+            defaultValue={[]}
+            onInitialize={handleInitialize}
+            tools={EDITOR_JS_TOOLS}
+          />
         </Box>
-      </Box>
+      </Paper>
       <Box>
         <Button variant="contained" onClick={handleSave}>
           Save
--- frontend/src/tools.ts
+++ frontend/src/tools.ts
@@ -1,1 +1,34 @@
-export const tools = {};
+// tools.js
+import Table from "@editorjs/table";
+import List from "@editorjs/list";
+import Image from "@editorjs/image";
+import SimpleImage from "@editorjs/simple-image";
+// import InlineCode from "@editorjs/inline-code";
+// import Embed from '@editorjs/embed'
+// import Warning from '@editorjs/warning'
+// import Code from '@editorjs/code'
+// import LinkTool from '@editorjs/link'
+// import Raw from '@editorjs/raw'
+// import Header from '@editorjs/header'
+// import Quote from '@editorjs/quote'
+// import Marker from '@editorjs/marker'
+// import CheckList from '@editorjs/checklist'
+// import Delimiter from '@editorjs/delimiter'
+
+export const EDITOR_JS_TOOLS = {
+  table: Table,
+  list: List,
+  image: SimpleImage,
+  //   inlineCode: InlineCode,
+  //   image: Image,
+  //   embed: Embed,
+  //   warning: Warning,
+  //   code: Code,
+  //   linkTool: LinkTool,
+  //   raw: Raw,
+  //   header: Header,
+  //   quote: Quote,
+  //   marker: Marker,
+  //   checklist: CheckList,
+  //   delimiter: Delimiter,
+};
