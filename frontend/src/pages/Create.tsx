e4bc3e95e038b5f57f0399b1cf0f46083f7e23e1
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-03-21 14:49:03 +0400

    Setup initial form content and metadata save

--- frontend/src/pages/Create.tsx
+++ frontend/src/pages/Create.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import { createReactEditorJS } from "react-editor-js";
-import { Formik, Field, Form, FormikHelpers } from "formik";
+import { Formik, Field, Form, FormikHelpers, useFormik } from "formik";
+import * as yup from "yup";
 
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Paper from "@mui/material/Paper";
 import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
 
 import Page from "components/Page";
 import PageHeading from "components/PageHeading";
@@ -20,58 +22,70 @@ interface IFormValues {
   title: string;
 }
 
-const initialFormValues: IFormValues = {
+const initialValues: IFormValues = {
   title: "",
 };
 
+const validationSchema = yup.object({
+  title: yup.string().required("Title is required"),
+});
+
 const Create = () => {
   const editorCore = React.useRef(null);
-  const [submitting, setSubmitting] = React.useState(false);
+
+  const handleFormSubmit = async (values: IFormValues) => {
+    const formData = values;
+    const contentData = await editorCore.current.save();
+    console.log(JSON.stringify({ formData, contentData }, null, 2));
+  };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema: validationSchema,
+    onSubmit: handleFormSubmit,
+  });
 
   const handleInitialize = React.useCallback((instance) => {
     editorCore.current = instance;
   }, []);
 
-  const handleSave = React.useCallback(async () => {
-    const savedData = await editorCore.current.save();
-    console.log(savedData);
-  }, []);
-
-  const handleFormSubmit = (values: IFormValues) => {
-    setTimeout(() => {
-      alert(JSON.stringify(values, null, 2));
-      setSubmitting(false);
-    }, 500);
-  };
-
   return (
     <Page>
       <PageHeading title="Create Post" />
       <Paper sx={{ my: 2, p: 2 }}>
-        <Formik initialValues={initialFormValues} onSubmit={handleFormSubmit}>
-          <Form>
+        <form onSubmit={formik.handleSubmit}>
+          <Stack spacing={2}>
             <TextField
-              id="outlined-basic"
-              label="Outlined"
-              variant="outlined"
+              fullWidth
+              id="title"
+              name="title"
+              label="Blog Title"
+              value={formik.values.title}
+              onChange={formik.handleChange}
+              error={formik.touched.title && Boolean(formik.errors.title)}
+              helperText={formik.touched.title && formik.errors.title}
             />
-
-            <button type="submit">Submit</button>
-          </Form>
-        </Formik>
-        <Box sx={{ my: 2 }}>
-          <ReactEditorJS
-            defaultValue={[]}
-            onInitialize={handleInitialize}
-            tools={EDITOR_JS_TOOLS}
-          />
-        </Box>
+            <Box sx={{ my: 2 }}>
+              <ReactEditorJS
+                defaultValue={[]}
+                onInitialize={handleInitialize}
+                tools={EDITOR_JS_TOOLS}
+              />
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "flex-end",
+              }}
+            >
+              <Button color="primary" variant="contained" type="submit">
+                Submit
+              </Button>
+            </Box>
+          </Stack>
+        </form>
       </Paper>
-      <Box>
-        <Button variant="contained" onClick={handleSave}>
-          Save
-        </Button>
-      </Box>
     </Page>
   );
 };
