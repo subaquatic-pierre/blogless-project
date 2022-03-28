5cb84145d7ed952cee7694c4b26c2b03dc0d2c0a
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-03-28 10:53:47 +0400

    Added BucketProxyException to bucket proxy

--- postmanager/proxy.py
+++ postmanager/proxy.py
@@ -4,45 +4,64 @@ import json
 import boto3
 
 
+class BucketProxyException(Exception):
+    pass
+
+
 class BucketProxyBase(ABC):
     def __init__(self, bucket_name, root_dir) -> None:
         self.bucket_name = bucket_name
         self.root_dir = root_dir
 
     def get_json(self, object_key):
-        object = self._configure_s3_object(object_key)
-        object_res = object.get()
+        try:
+            object = self._configure_s3_object(object_key)
+            object_res = object.get()
 
-        object_json = json.loads(object_res["Body"].read())
-        return object_json
+            object_json = json.loads(object_res["Body"].read())
+            return object_json
+        except Exception as e:
+            raise BucketProxyException(f"Error fething JSON from bucket, {str(e)}")
 
     def save_json(self, body: dict, filename: str):
-        self.bucket_interface.put_object(Key=self.root_dir)
-        self.bucket_interface.put_object(
-            Key=f"{self.root_dir}{filename}",
-            Body=json.dumps(body),
-        )
+        try:
+            self.bucket_interface.put_object(Key=self.root_dir)
+            self.bucket_interface.put_object(
+                Key=f"{self.root_dir}{filename}",
+                Body=json.dumps(body),
+            )
+        except Exception as e:
+            raise BucketProxyException(f"Error saving JSON to bucket, {str(e)}")
 
     def delete_files(self, filenames):
-        if len(filenames) > 0:
-            objects = [{"Key": filename} for filename in filenames]
-            self.bucket_interface.delete_objects(Delete={"Objects": objects})
+        try:
+            if len(filenames) > 0:
+                objects = [{"Key": filename} for filename in filenames]
+                self.bucket_interface.delete_objects(Delete={"Objects": objects})
+        except Exception as e:
+            raise BucketProxyException(f"Error deleting files from bucket, {str(e)}")
 
     def list_dir(self, dir: str = ""):
-        object_summary_iterator = self.bucket_interface.objects.all()
-        object_keys = [
-            obj.key
-            for obj in object_summary_iterator
-            if obj.key.startswith(f"{self.root_dir}{dir}")
-            and obj.key != f"{self.root_dir}{dir}"
-        ]
-        return object_keys
+        try:
+            object_summary_iterator = self.bucket_interface.objects.all()
+            object_keys = [
+                obj.key
+                for obj in object_summary_iterator
+                if obj.key.startswith(f"{self.root_dir}{dir}")
+                and obj.key != f"{self.root_dir}{dir}"
+            ]
+            return object_keys
+        except Exception as e:
+            raise BucketProxyException(f"Error listing files from bucket, {str(e)}")
 
     def save_bytes(self, body: bytes, filename: str):
-        self.bucket_interface.put_object(
-            Key=f"{self.root_dir}{filename}",
-            Body=body,
-        )
+        try:
+            self.bucket_interface.put_object(
+                Key=f"{self.root_dir}{filename}",
+                Body=body,
+            )
+        except Exception as e:
+            raise BucketProxyException(f"Error saving bytes to bucket, {str(e)}")
 
     @abstractmethod
     def _configure_s3_object(self, *args, **kwargs):
