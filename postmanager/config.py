011e7ca796a6953e6a3f752a11ffd30694466247
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-03-29 13:11:38 +0400

    Update setup config

--- postmanager/config.py
+++ postmanager/config.py
@@ -1,21 +1,11 @@
-from botocore.session import Session
+import boto3
 from botocore.config import Config
 
 
-def create_session(credentials={}):
-    # Add credentials to session
-    return Session()
-
-
-def create_client(session, config={}):
+def setup_client(credentials={}, client_config={}):
     default_config = Config(
         connect_timeout=5, read_timeout=60, retries={"max_attempts": 2}
     )
-    client = session.create_client("s3", config=default_config)
-    return client
 
-
-def setup_client(credentials={}, client_config={}):
-    session = create_session()
-    client = create_client(session)
+    client = boto3.client("s3", config=default_config)
     return client
